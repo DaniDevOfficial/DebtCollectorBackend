e76af5bf126121bbe87b5597c909de33e4c912ac
davidMac <bischof.david.db@gmail.com>
2025-10-17 08:06:36 +0200

    added pretty structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"dept-collector/internal/api"
+
+	_ "github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := api.NewRouter()
--- internal/api/Router.go
+++ internal/api/Router.go
@@ -1,5 +1,7 @@
 package api
 
+import "github.com/gin-gonic/gin"
+
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 
